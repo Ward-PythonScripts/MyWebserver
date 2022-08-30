40c2947a0bb53b2f9dbb608d762ebf4a8e7476a1
wardgeronimussmets <81919921+wardgeronimussmets@users.noreply.github.com>
2022-08-30 09:40:28 +0200

    back where we were

--- master/aviato/notifier/gamedeal_mailer.py
+++ master/aviato/notifier/gamedeal_mailer.py
@@ -1,3 +1,8 @@
+import email
+from email.message import EmailMessage
+import smtplib
+import ssl
+
 from . import credentials
 #credentials is just a py module that returns the needed strings like mail-address and password
 from . import notifier_backend
@@ -25,17 +30,33 @@ class GameDealMailSender:
         try:
             sorted_games = sorted(self.gamedeals,key=lambda x: (SORT_ORDER.get(str(x.cat).upper(),len(SORT_ORDER)),x.cat),reverse=False)
             
-            #build mail body
-            prevCat = ""
-            body = ""
-            for sorted_game in sorted_games:
-                if prevCat != sorted_game.cat:
-                    #new category
-                    body += "\n" + sorted_game.cat + "\n\n"
-                    prevCat = sorted_game.cat
-                body += sorted_game.title + "\n"
-                body += "https://www.reddit.com"+ sorted_game.reddit + "\n"
-            print(body)
+            for mail_rec in self.mails:
+                #build mail body
+                prevCat = ""
+                body = ""
+                for sorted_game in sorted_games:
+                    if prevCat != sorted_game.cat:
+                        #new category
+                        body += "\n" + sorted_game.cat + "\n\n"
+                        prevCat = sorted_game.cat
+                    body += sorted_game.title + "\n"
+                    body += "https://www.reddit.com"+ sorted_game.reddit + "\n"
+                
+                #build the email
+                em = EmailMessage()
+                em['From'] = self.email
+                em['To'] = mail_rec
+                em['Subject'] = "Gamedeals - New Games"
+                em.set_content(body)
+
+                context = ssl.create_default_context()
+
+                with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
+                    smtp.login(self.email,self.email_password)
+                    smtp.sendmail(self.email,mail_rec,em.as_string())
+                    smtp.close()
+
+
 
 
 
